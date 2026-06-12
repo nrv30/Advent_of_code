2d06c16eea1897f7a8a22b428707782726ef52a4
nrv <nirvairverma@gmail.com>
2026-06-12 01:24:54 -0700

    finished day 7

--- 2025/day7/Main.java
+++ 2025/day7/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,35 +33,72 @@ public class Main {
             return;
         }
 
-        Solution1.solve(grid);
+        //Solution1.solve(grid);
+        Solution1.solve2(grid);
 
     }
 
 // reference: https://aoc.winslowjosiah.com/solutions/2025/day/7/
-class Solution1 {
-    static void solve(List<String> grid) {
-        boolean [] beams = new boolean[grid.get(0).length()];
-        int splitCount = 0;
-
-        int start = grid.get(0).indexOf("S");
-        beams[start] = true;
-
-        for (int i = 0; i < grid.size(); ++i) {
-            // System.out.printf("------%d-----\n", i);
-            for (int j = 0; j < grid.get(0).length(); ++j) {
-                char c = grid.get(i).charAt(j);
-                // System.out.println(j);
-                if (!(c == '^' && beams[j]))
-                    continue;
-
-                splitCount++;
-                beams[j-1] = true;
-                beams[j+1] = true;
-                beams[j] = false;
+// https://joshcena.com/notes/aoc/2025/7/
+// these both took the same approach but copying the python code to java leads to a bug
+// because the original beams array shouldn't be modified while iterating
+// and rust code was easier for me to understand / similar to java
+// Problem statement was super confusing for me with this one idk
+    class Solution1 {
+        static long sum (long[] nums) {
+            long sum = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                sum += nums[i];
             }
+            return sum;
         }
-        System.out.println("Answer: " + splitCount);
+
+        static void solve(List<String> grid) {
+            boolean [] beams = new boolean[grid.get(0).length()];
+            int splitCount = 0;
+
+            int start = grid.get(0).indexOf("S");
+            beams[start] = true;
+
+            for (int i = 0; i < grid.size(); ++i) {
+                for (int j = 0; j < grid.get(0).length(); ++j) {
+                    char c = grid.get(i).charAt(j);
+                    if (!(c == '^' && beams[j]))
+                        continue;
+
+                    splitCount++;
+                    beams[j-1] = true;
+                    beams[j+1] = true;
+                    beams[j] = false;
+                }
+            }
+            System.out.println("Answer: " + splitCount);
+        }
+
+        static void solve2(List<String>grid) {
+            long[] beams = new long[grid.get(0).length()];
+            beams[grid.get(0).indexOf('S')] = 1;
+            for (int i = 1; i < grid.size(); ++i) {
+                long[] newBeams = new long[grid.get(i).length()];
+                for (int j = 0; j < newBeams.length; ++j) {
+                    char c = grid.get(i).charAt(j);
+                    if (c == '^') {
+                        // I don't think bounds checks are necesary for the input
+                        if (j > 0) {
+                            newBeams[j-1] += beams[j];
+                        }
+                        
+                        if (j + 1 < newBeams.length) {
+                            newBeams[j+1] += beams[j];
+                        }
+                    } else {
+                        newBeams[j] += beams[j];
+                    }
+                }
+                beams = newBeams;
+            }
+            System.out.println(sum(beams));
+        }
     }
-}
 
 }
