78a89cb7fa986ca214de13636847d8889353ce0b
nrv <nirvairverma@gmail.com>
2026-06-15 01:02:18 -0700

    implemented day 8 using union find

---
+++ 2025/day8/WeightedQuickUnionLib/WeightedQuickUnion.cs
@@ -0,0 +1,109 @@
+namespace WeightedQuickUnionLib;
+
+// A Weighted UnionFind implementation informed by the below video
+// No path compression
+// https://www.youtube.com/watch?v=xc9s9wdaSdU&t=95s
+public class WeightedQuickUnion<T> {
+
+    private Dictionary<T, int> elementsToIndex;
+    private int[]              parent;
+
+    public WeightedQuickUnion(T[] elements) 
+    { 
+        int size           = elements.Length;
+        parent             = new int[size];
+        elementsToIndex     = new Dictionary<T, int>();
+
+        for (int i = 0; i < size; ++i)
+        {
+            parent[i] = -1;
+            elementsToIndex.Add(elements[i], i);
+        }
+    }
+
+    private int Find(T elem)
+    {
+        int idx = elementsToIndex[elem];
+        int iParentIdx = parent[idx];
+        if (iParentIdx < 0)
+        {
+            return idx;
+        } 
+
+        return Find(iParentIdx); 
+    }
+
+    private int Find(int idx)
+    {
+        int ParentIdx = parent[idx];
+        if (ParentIdx < 0)
+        {
+            return idx;
+        } 
+
+        return Find(ParentIdx);
+    }
+
+    public bool Union(T a, T b)
+    {
+
+        int aParentIdx = Find(a);
+        int bParentIdx = Find(b);
+        if (aParentIdx == bParentIdx) return false;
+
+        int aParentWeight = Math.Abs(parent[aParentIdx]);
+        int bParentWeight = Math.Abs(parent[bParentIdx]);
+
+        if (aParentWeight >= bParentWeight)
+        {
+            parent[aParentIdx] -= bParentWeight;
+            parent[bParentIdx] = aParentIdx;
+        } else
+        {
+            parent[bParentIdx] -= aParentWeight;
+            parent[aParentIdx] = bParentIdx;
+        }
+        return true;
+    }
+
+    public bool IsConnected(T a, T b)
+    {
+        return Find(a) == Find(b);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(string.Join(" ", parent));
+ 
+    }
+
+    public Tuple<int, int, int> Get3LargestSets()
+    {
+        int m1, m2, m3;
+        m1=m2=m3=0;
+
+        for (int i = 0; i < parent.Length; ++i)
+        {
+            // we found a root note
+            if (parent[i] < 0)
+            {
+                int weight = Math.Abs(parent[i]);
+                if (weight > m1)
+                {
+                    m3 = m2;
+                    m2 = m1;
+                    m1 = weight;
+                } else if (weight > m2)
+                {
+                    m3 = m2;
+                    m2 = weight;
+                } else if (weight > m3)
+                {
+                    m3 = weight;
+                }
+            }
+        }
+
+        return Tuple.Create(m1, m2, m3);
+    }
+}
---
+++ 2025/day8/main.ps1
@@ -0,0 +1,69 @@
+function SqDist {
+    param (
+        $b1,
+        $b2
+        )
+        
+        $dx = $b1[0] - $b2[0]
+        $dy = $b1[1] - $b2[1]
+        $dz = $b1[2] - $b2[2]
+        
+        return ($dx*$dx + $dy*$dy + $dz*$dz)
+    }
+
+[System.Reflection.Assembly]::LoadFile("$PWD\WeightedQuickUnionLib\bin\Debug\net10.0\WeightedQuickUnionLib.dll")
+
+$boxes = [System.Collections.Generic.List[int[]]]::new()
+$pq    = [System.Collections.Generic.PriorityQueue[[System.Tuple[int32, int32]], int64]]::new() 
+    
+# parse input
+$lines = (get-content $($args[0]))
+$lines | ForEach-Object {
+   $point = $($_ -Split ',') | Foreach-Object {([int]::parse($_))}
+   $boxes.Add($point)
+}
+
+# a parallel array of indices to boxes
+$boxesIndices = 0..($boxes.Count-1) | ForEach-Object {$_}
+
+# build the pairs
+for ($i = 0; $i -lt $boxes.Count; $i++) {
+    $b1 = $boxes[$i]
+
+    for ($j = $i + 1; $j -lt $boxes.Count; $j++) {
+        $b2 = $boxes[$j]
+        $d = SqDist $b1 $b2
+        $pair = [System.Tuple]::Create($i, $j)
+        $pq.Enqueue($pair, $d)
+    }
+}
+
+# UnionFind of indices into $boxes
+$u = New-Object WeightedQuickUnionLib.WeightedQuickUnion[int](,$boxesIndices)
+
+# Solution 1
+if ($args[1] -eq 1) {
+    $n = 1000
+    for ($i = 0; $i -lt $n; $i++) {
+        $pair = $pq.Dequeue()
+        $null = $u.Union($pair.Item1, $pair.Item2)
+    }
+     
+    $top3Weights = $u.Get3LargestSets();
+    $answer = $top3Weights.Item1*$top3Weights.Item2*$top3Weights.Item3
+    write-host($answer)
+} else { # Solution 2
+    $count = $boxes.Count
+    $i, $j
+    do {
+        $pair = $pq.Dequeue()
+        $i = $pair.Item1
+        $j = $pair.Item2
+        $success = $u.Union($pair.Item1, $pair.Item2)
+        if ($success) {
+            $count--
+        }
+    } while ($count -gt 1)
+    write-host ($boxes[$i][0]*$boxes[$j][0])
+}
+# $u.Print()
